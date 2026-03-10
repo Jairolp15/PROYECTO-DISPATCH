cb47bebf0d61ef8873de319ba58ed4ffa4f26909
Jairolp15 <150107534+Jairolp15@users.noreply.github.com>
2026-03-10 00:58:17 -0400

    Delete PROYECTO/start.sh

--- PROYECTO/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Iniciar el bot de Telegram en segundo plano
-python telegram_bot.py &
-
-# Iniciar la aplicación Streamlit
-streamlit run app.py --server.port $PORT --server.address 0.0.0.0
